2ed9f7b1c7d830f4b4399117a8fa721d9aa4966f
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2023-11-19 22:22:37 +0530

    Smooth scroll

--- app/page.jsx
+++ app/page.jsx
@@ -7,11 +7,23 @@ import OurServices from './components/OurServices'
 import Distinction from './components/Distinction'
 import OurImpact from './components/OurImpact'
 import OurClients from './components/OurClients'
+import Lenis from '@studio-freight/lenis'
+import { useEffect } from 'react';
 
 import { Poppins } from 'next/font/google';
 const poppins = Poppins({weight:["100",'200','300','400','500','600','700','800','900'],subsets:['latin']})
 
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis()
+    
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+  })
   return (
     <div className={poppins.className + ' bg-bgpurple scroll-smooth'}>
       <Hero />
